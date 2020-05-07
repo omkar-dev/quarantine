fd217ec4aafd02571a92f129db2dddd29c8f2162
Samuel Fernandes <36688895+samuelfernandes9@users.noreply.github.com>
2020-05-07 18:29:03 +0530

    need help conditional forms and validation

--- quarantine/src/app/pages/need-help/need-help.module.ts
+++ quarantine/src/app/pages/need-help/need-help.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -16,6 +16,7 @@ import { NeedHelpPage } from './need-help.page';
     IonicModule,
     NeedHelpPageRoutingModule,
     TranslateModule,
+    ReactiveFormsModule
 
   ],
   declarations: [NeedHelpPage]
